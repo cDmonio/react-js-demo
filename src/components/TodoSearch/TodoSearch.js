73a1bb39d3dc114e30d8532a3b2ecec680eff2aa
Joe <joegv7@gmail.com>
2022-03-24 07:54:22 +0100

    hook cambio de estado search

--- src/components/TodoSearch/TodoSearch.js
+++ src/components/TodoSearch/TodoSearch.js
@@ -3,17 +3,23 @@ import './TodoSearch.css'
 
 function TodoSearch()
 {
+    //REACT HOOK
+    const [search, setSearch] = React.useState('');
+
     const onChangeSearchValue = (event) => {
         console.log(event.target.value);
+        setSearch(event.target.value);
     };
 
-    return (
+    return [
         <input 
             className="TodoSearch" 
-            placeholder="Prueba" 
+            placeholder="Prueba"
+            value={search}
             onChange={onChangeSearchValue}
-        />
-    );
+        />,
+        <p>{search}</p>
+    ];
 }
 
 export { TodoSearch };
